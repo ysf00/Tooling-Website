6f8981878718fbf0347a3e4b90ca3261fbb8eb86
ysf00 <77760819+ysf00@users.noreply.github.com>
2021-03-24 05:02:44 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 session_start();
 
 // connect to database
-$db = mysqli_connect('172.31.36.6', 'webaccess', 'ade12345@', 'tooling');
+$db = mysqli_connect('54.172.128.37', 'webaccess', 'ade12345@', 'tooling');
 
 // Check connection
 // if (mysqli_connect_errno()) {
